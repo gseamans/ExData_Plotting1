ba4c887724a758072acf65cd217ef5fa32312876
Gary R Seamans <seamans@pobox.com>
2017-08-12 21:26:52 -0700

    Final commit. Added plot4.png and updates to the CodeBook

--- plot3.R
+++ plot3.R
@@ -17,4 +17,6 @@ png(filename = "plot3.png", width = 480, height = 480, units = "px", pointsize =
 plot(as.POSIXlt(powerData$DateTime), powerData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(as.POSIXlt(powerData$DateTime), powerData$Sub_metering_2, type="l", col = "red")
 lines(as.POSIXlt(powerData$DateTime), powerData$Sub_metering_3, type="l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -12,5 +12,19 @@ powerData <- unite(powerData, DateTime, c(Date, Time), remove = TRUE, sep =":")
 # Now change the Date values into date types
 powerData %<>% mutate(DateTime=dmy_hms(DateTime))
 
-# Create an write out the plot
+# Set up the rows/columns
 png(filename = "plot4.png", width = 480, height = 480, units = "px", pointsize = 12, bg="white")
+par(mfrow = c(2,2))
+# Print Top Left
+plot(as.POSIXlt(powerData$DateTime), powerData$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+# Print Top Right
+plot(as.POSIXlt(powerData$DateTime), powerData$Voltage, type="l", xlab="datetime", ylab="Voltage")
+# Print Bottom Left
+plot(as.POSIXlt(powerData$DateTime), powerData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(as.POSIXlt(powerData$DateTime), powerData$Sub_metering_2, type="l", col = "red")
+lines(as.POSIXlt(powerData$DateTime), powerData$Sub_metering_3, type="l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty=1, lwd=2.5, col=c("black", "red", "blue"), bty = "n")
+# Print Bottom Right       
+plot(as.POSIXlt(powerData$DateTime), powerData$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
