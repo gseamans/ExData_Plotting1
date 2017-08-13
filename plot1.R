52b33aa2ed1b01c47a813b993981ba62596acd34
Gary R Seamans <seamans@pobox.com>
2017-08-12 19:01:06 -0700

    Complte plot1, stubs for plots 2-4

---
+++ plot1.R
@@ -0,0 +1,18 @@
+# Code for plot1
+library(lubridate)
+library(dplyr)
+library(sqldf)
+powerData <- 
+    read.csv.sql(file = "household_power_consumption.txt",
+                 sql = "select * from file where [Date] == '1/2/2007' or [Date] == '2/2/2007'", sep = ';')
+# The two colums *Date* and *Time* would be better combined and converted to a date/time.
+powerData <- unite(powerData, DateTime, c(Date, Time), remove = TRUE, sep =":")
+
+# Now change the Date values into date types
+powerData %<>% mutate(DateTime=mdy_hms(DateTime))
+
+# Create an write out the plot
+png(filename = "plot1.png", width = 480, height = 480, units = "px", pointsize = 12, bg="white")
+hist(powerData$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency", main = "Global Active Power")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,15 @@
+# Code for plot2
+library(lubridate)
+library(dplyr)
+library(sqldf)
+powerData <- 
+    read.csv.sql(file = "household_power_consumption.txt",
+                 sql = "select * from file where [Date] == '1/2/2007' or [Date] == '2/2/2007'", sep = ';')
+# The two colums *Date* and *Time* would be better combined and converted to a date/time.
+powerData <- unite(powerData, DateTime, c(Date, Time), remove = TRUE, sep =":")
+
+# Now change the Date values into date types
+powerData %<>% mutate(DateTime=mdy_hms(DateTime))
+
+# Create an write out the plot
+png(filename = "plot2.png", width = 480, height = 480, units = "px", pointsize = 12, bg="white")
